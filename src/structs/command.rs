543017ff13cb952e71dafc5b818535f1ea6a2d3d
Florian <me@fgardt.dev>
2024-05-14 02:46:21 +0200

    Add User Apps

--- src/structs/command.rs
+++ src/structs/command.rs
@@ -127,6 +127,10 @@ pub struct Command<U, E> {
     pub context_menu_name: Option<String>,
     /// Whether responses to this command should be ephemeral by default (application-only)
     pub ephemeral: bool,
+    /// List of installation contexts for this command (application-only)
+    pub install_context: Option<Vec<serenity::InstallationContext>>,
+    /// List of interaction contexts for this command (application-only)
+    pub interaction_context: Option<Vec<serenity::InteractionContext>>,
 
     // Like #[non_exhaustive], but #[poise::command] still needs to be able to create an instance
     #[doc(hidden)]
@@ -201,7 +205,17 @@ impl<U, E> Command<U, E> {
         }
 
         if self.guild_only {
-            builder = builder.dm_permission(false);
+            builder = builder.contexts(vec![serenity::InteractionContext::Guild]);
+        } else if self.dm_only {
+            builder = builder.contexts(vec![serenity::InteractionContext::BotDm]);
+        }
+
+        if let Some(install_context) = self.install_context.clone() {
+            builder = builder.integration_types(install_context);
+        }
+
+        if let Some(interaction_context) = self.interaction_context.clone() {
+            builder = builder.contexts(interaction_context);
         }
 
         if self.subcommands.is_empty() {
@@ -235,7 +249,17 @@ impl<U, E> Command<U, E> {
         });
 
         if self.guild_only {
-            builder = builder.dm_permission(false);
+            builder = builder.contexts(vec![serenity::InteractionContext::Guild]);
+        } else if self.dm_only {
+            builder = builder.contexts(vec![serenity::InteractionContext::BotDm]);
+        }
+
+        if let Some(install_context) = self.install_context.clone() {
+            builder = builder.integration_types(install_context);
+        }
+
+        if let Some(interaction_context) = self.interaction_context.clone() {
+            builder = builder.contexts(interaction_context);
         }
 
         Some(builder)
